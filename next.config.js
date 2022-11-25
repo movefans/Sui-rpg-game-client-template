10a9b5330113c543f187a57f0a513236bb3c9910
henryliu <17600047539@163.com>
2022-11-25 10:35:41 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-const { i18n } = require('./next-i18next.config');
 
 module.exports = {
-    experimental: {
-        outputStandalone: true,
-    },
+    // experimental: {
+    //     output: true,
+    // },
     eslint: {
         dirs: ['src'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
     },
-    i18n,
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,10 @@
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
-import { appWithTranslation } from 'next-i18next'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <Component {...pageProps} />
   )
 }
-export default  appWithTranslation(MyApp)
 
+export default  MyApp
