381bf11856df59c24f1ba28cb13dc77ab911496b
henryliu <17600047539@163.com>
2022-11-25 10:48:41 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,39 @@ module.exports = {
     darkMode: 'media', // or 'media' or 'class'
     theme: {
         extend: {
+            screens:{
+                "small":"375px",
+                "xs" :"400px",
+            },
             fontFamily: {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
+            height: {
+                lg: '30rem',
+                xl:'34rem',
+                big:'40rem',
+            },
+            spacing:{
+                '18':'4.5rem',
+                "85":"21.5rem",
+                '90':"23rem",
+                "97":"28rem",
+                '99': "32rem",
+                '100':"33rem",
+                '104':"38rem",
+            },
+
         },
         variants: {
             extend: {},
         },
-        plugins: [],
+
+    },
+    plugins: [
+        require('tailwind-scrollbar'),
+        require('@tailwindcss/line-clamp'),
+    ],
+    variants: {
+        scrollbar: ['rounded']
     }
 }
